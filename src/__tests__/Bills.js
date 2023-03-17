e45dfaf13ed00be6446eb6bacf411fc3b6c00c00
ClementAver <clement.aver@yahoo.fr>
2023-03-17 09:40:45 +0100

    fixes dependencies bugs and refactor first issue (bug report - Bills).

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -3,12 +3,15 @@
  */
 
 import { screen, waitFor } from "@testing-library/dom";
-import BillsUI from "../views/BillsUI.js";
+// import BillsUI from "../views/BillsUI.js";
 import { bills } from "../fixtures/bills.js";
-import { ROUTES_PATH } from "../constants/routes.js";
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
+import mockStore from "../__mocks__/store";
 
 import router from "../app/Router.js";
+import userEvent from "@testing-library/user-event";
+import Bills from "../containers/Bills.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -29,11 +32,17 @@ describe("Given I am connected as an employee", () => {
       const windowIcon = screen.getByTestId("icon-window");
       //to-do write expect expression
     });
-    test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills });
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map((a) => a.innerHTML);
-      const antiChrono = (a, b) => (a < b ? 1 : -1);
-      const datesSorted = [...dates].sort(antiChrono);
+
+    test("Then bills should be ordered from earliest to latest", async () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+
+      let bills = new Bills({ document, onNavigate, store: mockStore, localStorage: null });
+      let sortedBills = await bills.getBills();
+      const dates = sortedBills.map((bill) => bill.date);
+      const antiChrono = (a, b) => (a.date < b.date ? 1 : -1);
+      const datesSorted = dates.sort(antiChrono);
       expect(dates).toEqual(datesSorted);
     });
   });
